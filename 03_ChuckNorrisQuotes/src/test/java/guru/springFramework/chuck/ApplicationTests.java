1563dacd6e733a93f4b996efab89f3e3efaa6fb3
fosicata1996 <fosalaucatalin@gmail.com>
2021-02-13 20:03:28 +0200

    03_ChuckNorrisQuotes

---
+++ 03_ChuckNorrisQuotes/src/test/java/guru/springFramework/chuck/ApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springFramework.chuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests
+{
+	
+	@Test
+	void contextLoads()
+	{
+	}
+	
+}
