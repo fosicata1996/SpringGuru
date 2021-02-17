82c856f2cd4bb54d6a13e0f317ea52b3ebb72722
fosicata1996 <fosalaucatalin@gmail.com>
2021-02-17 20:17:25 +0200

    Data source properties file

--- 02_DependencyInjection/src/main/java/guru/springframework/dependency_injection/DependencyInjectionApplication.java
+++ 02_DependencyInjection/src/main/java/guru/springframework/dependency_injection/DependencyInjectionApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import guru.springframework.dependency_injection.controllers.ConstructorInjectedController;
-import guru.springframework.dependency_injection.controllers.I18nController;
 import guru.springframework.dependency_injection.controllers.MyController;
-import guru.springframework.dependency_injection.controllers.PropertyInjectedController;
-import guru.springframework.dependency_injection.controllers.SetterInjectedController;
+import guru.springframework.dependency_injection.examplebeans.FakeDataSource;
 
 @SpringBootApplication
 public class DependencyInjectionApplication
@@ -20,28 +17,9 @@ public class DependencyInjectionApplication
 		
 		MyController myController = (MyController) ctx.getBean("myController");
 		
-		System.out.println(">>>>>> Primary Bean <<<<<");
-		System.out.println(myController.sayHello());
+		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 		
-		System.out.println("--------------------------------------------------");
-		
-		System.out.println(">>>>> Property <<<<<");
-		PropertyInjectedController propertyC = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-		System.out.println(propertyC.getGreeting());
-		
-		System.out.println(">>>>> Setter <<<<<");
-		SetterInjectedController setterC = (SetterInjectedController) ctx.getBean("setterInjectedController");
-		System.out.println(setterC.getGreeting());
-		
-		System.out.println(">>>>> Constructor <<<<<");
-		ConstructorInjectedController constructorC = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-		System.out.println(constructorC.getGreeting());
-		
-		System.out.println("--------------------------------------------------");
-		
-		System.out.println("--> @Profile");
-		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-		System.out.println(i18nController.sayHello());
+		System.out.println("######## " + fakeDataSource.getUser());
 		
 	}
 	
