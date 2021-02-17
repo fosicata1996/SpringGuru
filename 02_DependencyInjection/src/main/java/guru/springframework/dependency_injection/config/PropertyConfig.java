1fd0e2db83d9279882df41b107d26eb28174e54e
fosicata1996 <fosalaucatalin@gmail.com>
2021-02-17 20:56:00 +0200

    Data source properties file - move to application.properties

--- 02_DependencyInjection/src/main/java/guru/springframework/dependency_injection/config/PropertyConfig.java
+++ 02_DependencyInjection/src/main/java/guru/springframework/dependency_injection/config/PropertyConfig.java
@@ -1,28 +1,16 @@
 package guru.springframework.dependency_injection.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 import guru.springframework.dependency_injection.examplebeans.FakeDataSource;
 import guru.springframework.dependency_injection.examplebeans.FakeJmsBroker;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-		@PropertySource("classpath:datasource.properties"),
-		@PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig
 {
-	@Autowired
-	Environment env;
-	
 	@Value("${guru.username}")
 	String user;
 	
@@ -45,7 +33,7 @@ public class PropertyConfig
 	public FakeDataSource fakeDataSource()
 	{
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUser(env.getProperty("USERNAME"));
+		fakeDataSource.setUser(user);
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
