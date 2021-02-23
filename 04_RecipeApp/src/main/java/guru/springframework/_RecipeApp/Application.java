e4cec7c8fc81568a1a7de2a84358b5eeee96376d
fosicata1996 <fosalaucatalin@gmail.com>
2021-02-23 19:30:56 +0200

    04_RecipeApp

---
+++ 04_RecipeApp/src/main/java/guru/springframework/_RecipeApp/Application.java
@@ -0,0 +1,15 @@
+package guru.springframework._RecipeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application
+{
+	
+	public static void main(String[] args)
+	{
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
---
+++ 04_RecipeApp/src/test/java/guru/springframework/_RecipeApp/ApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springframework._RecipeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests
+{
+	
+	@Test
+	void contextLoads()
+	{
+	}
+	
+}
