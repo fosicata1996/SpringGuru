51d8ad144682f6beaac803db41d0ac5b072f3c67
fosicata1996 <fosalaucatalin@gmail.com>
2021-03-24 15:46:31 +0200

    04_RecipeApp - Upload Image - Persisting in DB

---
+++ 04_RecipeApp/src/test/java/guru/springframework/services/ImageServiceImplTest.java
@@ -0,0 +1,58 @@
+package guru.springframework.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.repositories.RecipeRepository;
+
+public class ImageServiceImplTest
+{
+	@Mock
+	RecipeRepository recipeRepository;
+	
+	ImageService imageService;
+	
+	@BeforeEach
+	void setUp()
+	{
+		MockitoAnnotations.initMocks(this);
+		
+		imageService = new ImageServiceImpl(recipeRepository);
+	}
+	
+	@Test
+	void saveImageFile() throws IOException
+	{
+		Long id = 1L;
+		MultipartFile multipartFile = new MockMultipartFile("imageFile", "testing.txt", "text/plain",
+				"Spring Framework Guru".getBytes());
+		
+		Recipe recipe = new Recipe();
+		recipe.setId(id);
+		Optional<Recipe> recipeOptional = Optional.of(recipe);
+		
+		when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+		
+		ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+		
+		imageService.saveImageFile(id, multipartFile);
+		
+		verify(recipeRepository).save(argumentCaptor.capture());
+		Recipe savedRecipe = argumentCaptor.getValue();
+		assertEquals(multipartFile.getBytes().length, savedRecipe.getImage().length);
+	}
+}
